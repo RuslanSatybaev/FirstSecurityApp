99e629530b53c3a87fb46ecf118264a92c150baa
Ruslan Satybaev <medtehnik850@gmail.com>
2023-10-16 21:53:35 +0700

    Lesson85: Safety at CSRF

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
